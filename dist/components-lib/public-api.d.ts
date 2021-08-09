cf9b43d963e70ff590a1f4046146b874be8092ee
Jose Ferreira da Silva <ferreirajosejp@gmail.com>
2021-08-09 11:00:19 -0300

    gerando nova versao

---
+++ dist/components-lib/public-api.d.ts
@@ -0,0 +1,4 @@
+export * from './lib/components-lib.service';
+export * from './lib/components-lib.component';
+export * from './lib/components-lib.module';
+export * from './lib/interface/show.interface';
