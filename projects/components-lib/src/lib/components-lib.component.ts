d638a52f653ec2b3cf126148f06c1fa380744496
Jose Ferreira da Silva <ferreirajosejp@gmail.com>
2021-08-09 10:57:29 -0300

    removendo componente sem uso

--- projects/components-lib/src/lib/components-lib.component.ts
+++ projects/components-lib/src/lib/components-lib.component.ts
@@ -1,20 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { ComponentsLibService } from "./components-lib.service";
 
 @Component({
   selector: 'uic-components-lib',
-  template: `
-    <p>
-      components-lib works!
-    </p>
-  `,
+  template: `<img *ngIf="posterUrl$ | async as src" [src]="src" />`,
   styles: [
+    `
+  :host {
+    display: inline-block;
+    overflow: hidden;
+    border-radius: 4px;
+    line-height: 0;
+  }
+  `
   ]
 })
 export class ComponentsLibComponent implements OnInit {
 
-  constructor() { }
+  @Input() showId: number;
+  posterUrl$: Observable<string>;
 
-  ngOnInit(): void {
+  constructor(private tvmaze: ComponentsLibService) {}
+
+  ngOnInit() {
+    this.posterUrl$ = this.tvmaze
+      .getShow(this.showId)
+      .pipe(map(show => show.image.medium));
   }
 
 }
--- projects/components-lib/src/public-api.ts
+++ projects/components-lib/src/public-api.ts
@@ -3,6 +3,6 @@
  */
 
 export * from './lib/components-lib.service';
-export * from './lib/poster/poster.component';
+export * from './lib/components-lib.component';
 export * from './lib/components-lib.module';
 export * from './lib/interface/show.interface';
