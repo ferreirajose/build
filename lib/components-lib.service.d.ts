f01a9abb578dc6e9905573074941b6d3a8afc3bb
Jose Ferreira da Silva <ferreirajosejp@gmail.com>
2021-08-09 11:24:06 -0300

    gerando nova versao

--- lib/components-lib.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { Show } from "./interface/show.interface";
-export declare class ComponentsLibService {
-    private http;
-    private readonly apiRoot;
-    constructor(http: HttpClient);
-    getShow(id: number): Observable<Show>;
-}
--- public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/components-lib.service';
-export * from './lib/components-lib.component';
-export * from './lib/components-lib.module';
-export * from './lib/interface/show.interface';
